9c792027a80986f61bc9586052009146783580af
ima17 <ima.sudaraka@outlook.com>
2022-12-26 02:00:54 +0530

    Refactor the code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,6 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
-function createEntry(emojiTerm) {
-  return (
-    <Entry
-      key={emojiTerm.id}
-      emoji={emojiTerm.emoji}
-      name={emojiTerm.name}
-      meaning={emojiTerm.meaning}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
@@ -20,7 +9,16 @@ function App() {
         <span>emojipedia</span>
       </h1>
 
-      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((emojiTerm) => (
+          <Entry
+            key={emojiTerm.id}
+            emoji={emojiTerm.emoji}
+            name={emojiTerm.name}
+            meaning={emojiTerm.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
